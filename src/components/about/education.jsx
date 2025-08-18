4da087b3a22bba3be68e2a96e8b5240fa6358de7
sampanna17 <sampannapiya13@gmail.com>
2025-08-18 16:22:45 +0545

    Updated education for mobile view

--- src/components/about/education.jsx
+++ src/components/about/education.jsx
@@ -80,8 +80,8 @@ const EducationSection = ({ containerVariants, itemVariants }) => {
                     {item.icon}
                   </Motion.span>
                   <div>
-                    <h3 className="text-lg font-medium text-gray-900">{item.institution}</h3>
-                    <p className="text-gray-600">{item.period}</p>
+                    <h3 className="text-sm lg:text-lg font-medium text-gray-900">{item.institution}</h3>
+                    <p className="text-sm lg:text-lg text-gray-600">{item.period}</p>
                   </div>
                 </div>
               </Motion.button>
@@ -94,7 +94,7 @@ const EducationSection = ({ containerVariants, itemVariants }) => {
           {educationData.map((item) => (
             <Motion.button
               key={item.id}
-              className={`px-8 py-4 border-b-2 transition-colors ${activeTab === item.id ? 'border-gray-900 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700'}`}
+              className={`px-8 py-4 border-b-2 transition-colors ${activeTab === item.id ? 'border-gray-900 text-gray-900   '  : 'border-transparent text-gray-500 hover:text-gray-700'}`}
               onClick={() => setActiveTab(item.id)}
               variants={itemVariants}
               whileHover={{ scale: 1.05 }}
@@ -125,16 +125,16 @@ const EducationSection = ({ containerVariants, itemVariants }) => {
                 >
                   {educationData[activeTab].icon}
                 </Motion.div>
-                <h3 className="text-2xl font-medium text-gray-900 mb-2">
+                <h3 className="text-xl lg:text-2xl font-medium text-gray-900 mb-2">
                   {educationData[activeTab].institution}
                 </h3>
-                <p className="text-gray-500">{educationData[activeTab].period}</p>
+                <p className="text-gray-500 text-sm lg:text-lg">{educationData[activeTab].period}</p>
               </div>
               <div className="md:w-2/3">
-                <h4 className="text-xl font-medium text-gray-800 mb-4">
+                <h4 className="text-base lg:text-xl font-medium text-gray-800 mb-4">
                   {educationData[activeTab].degree}
                 </h4>
-                <p className="text-gray-600 leading-relaxed mb-6">
+                <p className="text-sm lg:text-lg text-gray-600 leading-relaxed mb-6">
                   {educationData[activeTab].description}
                 </p>
                 <Motion.div 
